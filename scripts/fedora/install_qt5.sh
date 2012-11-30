56b96e7fe93178a720b7d6b19946084f4603476d
Alexandre Moore <amoore@ics.com>
2012-11-30 10:48:12 -0500

    update qt5 scritps

--- scripts/fedora/install_qt5.sh
+++ scripts/fedora/install_qt5.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-
+QT5_INSTALL_PATH='~/Documents/git_repo/'
 
 su -c '
 yum install -y flex bison gperf libicu-devel libxslt-devel ruby perl-version at-spi2-core-devel
 yum --enablerepo=updates install libxcb libxcb-devel xcb-util xcb-util-devel xcb-util-*-devel
 '
 
-cd ~/Documents/git_repo/
+cd $QT5_INSTALL_PATH
 rm -rvf qt5
 git clone git://gitorious.org/qt/qt5.git
 cd qt5
--- scripts/update_qt5.sh
+++ scripts/update_qt5.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-#clean repo && git pull && 
+QT5_PATH='~/Documents/git_repo/qt5'
 
-cd ~/Documents/git_repo/qt5
+#clean repo && git pull && 
+cd $QT5_PATH
 git submodule update --recursive && ./configure -confirm-license -developer-build -opensource -nomake examples -nomake tests -no-dbus -no-gtkstyle && make -j 8 && make install

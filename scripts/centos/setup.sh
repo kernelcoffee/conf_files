eb4104ac096d08998fbd01a1510034fb949e21c7
Alexandre Moore <amoore@ics.com>
2012-11-30 10:48:41 -0500

    update centos setup script

--- scripts/centos/setup.sh
+++ scripts/centos/setup.sh
@@ -2,42 +2,9 @@
 
 #meant to be run as root
 
-USERNAME=''
-PASSWD=''
-
-RPMFORGEPKG='rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm'
-RPMFORGEADDR='http://packages.sw.be/rpmforge-release/'
-
-EPELPKG='epel-release-6-7.noarch.rpm'
-EPELADDR='http://dl.fedoraproject.org/pub/epel/6/x86_64/'
-
-REMIPKG='remi-release-6.rpm'
-REMIADDR='http://rpms.famillecollet.com/enterprise/'
-
-yum update
-yum -y groupinstall "Development Tools"
-yum -y install ntp wget
-ntpdate pool.ntp.org
-
-#install rpmforge repos
-wget $RPMFORGEADDR$RPMFORGEPKG
-rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
-rpm -K $RPMFORGEPKG
-rpm -i $RPMFORGEPKG
-
-#install EPEL repos
-wget $EPELADDR$EPELPKG
-rpm -i $EPELPKG
-
-#install remi repo (PHP)
-wget $REMIADDR$REMIPKG
-rpm -i $REMIPKG
-
-yum -y install curl htop screen java-1.7.0-openjdk gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel mysql mysql-devel emacs-nox mysql-server
-
-#adduser $USERNAME
-#passwd $PASSWD
-#usermod -G wheel $USERNAME
+./init_repo.sh
+./install_base_pkg.sh
+./init_default_user.sh
 
 mkdir -p /home/shared/downloads/inprogress
 mkdir -p /home/shared/backup

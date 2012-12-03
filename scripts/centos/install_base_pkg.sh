a29edd80af5e40d6ed355f1cfd385432caf9801a
Alexandre Moore <amoore@ics.com>
2012-12-03 15:01:14 -0500

    update centos_base_pkg

--- scripts/centos/install_base_pkg.sh
+++ scripts/centos/install_base_pkg.sh
@@ -7,4 +7,3 @@ ntpdate pool.ntp.org
 
 yum -y install curl htop screen java-1.7.0-openjdk gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel mysql mysql-devel emacs-nox mysql-server pcre pcre-devel php-fpm openssl openssl-devel emacs-auto-complete emacs-color-theme emacs-el emacs-goodies colordiff unzip perl-version lm_sensors
 
-pecl install apc

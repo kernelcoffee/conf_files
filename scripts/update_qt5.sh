83b6b6e77322dccb7a2329bc9ece2945957d1727
Alexandre Moore <amoore@ics.com>
2013-03-05 10:23:36 -0500

    update flexget config file / setup_fedora and the update qt5 scripts

--- scripts/update_qt5.sh
+++ scripts/update_qt5.sh
@@ -3,4 +3,4 @@
 QT5_PATH=~/Documents/git_repo/qt5/
 
 #clean repo && git pull && 
-cd $QT5_PATH && git submodule foreach --recursive "git clean -dfx" && git submodule update --recursive && ./configure -confirm-license -developer-build -opensource -nomake examples -nomake tests -no-dbus -no-gtkstyle && make -j 8 && make install
+cd $QT5_PATH && git submodule foreach --recursive "git clean -dfx" && git submodule update --recursive && ./configure -confirm-license -developer-build -opensource -nomake examples -nomake tests -no-gtkstyle && make -j 8 && make install

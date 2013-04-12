c926386358a69e0814a9fde41e0fe159224a0af4
Alexandre Moore <amoore@ics.com>
2013-04-12 15:51:31 -0400

    update install scripts

--- scripts/centos/install_base_pkg.sh
+++ scripts/centos/install_base_pkg.sh
@@ -5,5 +5,4 @@ yum -y groupinstall "Development Tools"
 yum -y install ntp wget
 ntpdate pool.ntp.org
 
-yum -y install curl htop screen java-1.7.0-openjdk gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel mysql mysql-devel emacs-nox mysql-server pcre pcre-devel php-fpm openssl openssl-devel emacs-auto-complete emacs-color-theme emacs-el emacs-goodies colordiff unzip perl-version lm_sensors
-
+yum -y install curl htop screen java-1.7.0-openjdk gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel mysql mysql-devel emacs-nox mysql-server pcre pcre-devel php-fpm openssl openssl-devel emacs-auto-complete emacs-color-theme emacs-el emacs-goodies colordiff unzip perl-version lm_sensors ImageMagick-devel

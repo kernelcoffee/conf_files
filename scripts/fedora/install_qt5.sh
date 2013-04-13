7c4a03a6c13445139b620a3333c33c2570152b10
Alexandre Moore <greys@vaio.kernelcoffee>
2013-04-13 19:45:51 -0400

    update for fedora 18

--- scripts/fedora/install_qt5.sh
+++ scripts/fedora/install_qt5.sh
@@ -3,8 +3,7 @@
 QT5_INSTALL_PATH='~/Documents/git_repo/'
 
 su -c '
-yum install -y flex bison gperf libicu-devel libxslt-devel ruby perl-version at-spi2-core-devel
-yum --enablerepo=updates install libxcb libxcb-devel xcb-util xcb-util-devel xcb-util-*-devel
+yum install -y flex bison gperf libicu-devel libxslt-devel ruby perl-version at-spi2-core-devel libxcb libxcb-devel xcb-util xcb-util-devel xcb-util-*-devel
 '
 
 cd $QT5_INSTALL_PATH
@@ -12,4 +11,4 @@ rm -rvf qt5
 git clone git://gitorious.org/qt/qt5.git
 cd qt5
 perl init-repository
-./configure -confirm-license -developer-build -opensource -nomake examples -nomake tests -no-dbus -no-gtkstyle && make -j 8 && make install
+./configure -confirm-license -developer-build -opensource -nomake examples -nomake tests -no-gtkstyle && make -j 8 && make install

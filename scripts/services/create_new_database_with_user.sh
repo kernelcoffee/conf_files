fa77431f2e7f8cb8f8111f49fc5490b2e928ea4a
Alexandre Moore <amoore@ics.com>
2013-09-10 17:00:56 -0400

    update sript

--- scripts/services/create_new_database_with_user.sh
+++ scripts/services/create_new_database_with_user.sh
@@ -1,11 +1,15 @@
-PHP_USER='apache'
+#!/bin/sh
+
 DB_NAME='database'
 DB_USER=$DB_NAME
 DB_PASSWORD='password'
 
 echo "
-CREATE USER 'piwik'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'piwik'@'localhost' WITH GRANT OPTION;
+DROP DATABASE IF EXISTS $DB_NAME;
+CREATE DATABASE $DB_NAME;
+DROP USER '$DB_USER'@'localhost';
+CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost' WITH GRANT OPTION;
 " > request.sql
 
 mysql -u root -p < request.sql

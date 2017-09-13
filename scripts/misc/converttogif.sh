e3338703f64f5c3f5921adee98669237a0e36d9d
Alexandre Moore <alexandre@kernelcoffee.org>
2017-09-12 23:27:35 -0700

    Add convert video to gif script

---
+++ scripts/misc/converttogif.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+inputFile=$1
+
+FPS=15
+WIDTH=480
+
+#Generate palette for better quality
+ffmpeg -i $inputFile -vf fps=$FPS,scale=$WIDTH:-1:flags=lanczos,palettegen tmp_palette.png
+
+#Generate gif using palette
+ffmpeg -i $inputFile -i tmp_palette.png -loop 0 -filter_complex "fps=$FPS,scale=$WIDTH:-1:flags=lanczos[x];[x][1:v]paletteuse" output.gif

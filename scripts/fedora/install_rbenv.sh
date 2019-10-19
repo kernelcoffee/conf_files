706bc506680d026d139e4bada046f75e36e38ac6
Alexandre Moore <alexandre@kernelcoffee.org>
2019-10-18 22:29:44 -0700

    Fall's cleaning

--- scripts/fedora/install_rbenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd $HOME
-git clone git://github.com/sstephenson/rbenv.git .rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-mkdir -p ~/.rbenv/plugins
-git clone git://github.com/sstephenson/ruby-build.git  ~/.rbenv/plugins/ruby-build
-git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
--- scripts/raspi/setup_devpi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-apt-get update
-apt-get install git emacs-nox
